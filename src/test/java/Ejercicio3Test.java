2fec9dbd3cfff9f25654b712698998d6612b958a
recruz79 <raul.cruz79@gmail.com>
2020-05-30 20:55:28 -0300

    deu

---
+++ src/test/java/Ejercicio3Test.java
@@ -0,0 +1,55 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+@RunWith(SpringRunner.class)
+public class Ejercicio3Test {
+
+    Ejercicio3 ejercicio3 = new Ejercicio3();
+
+    @Test
+    public void verificarOracionConCincoPalabras() {
+        String sentence = "Hola mi nombre es robot";
+
+        List result = ejercicio3.splitSentence(sentence);
+        assertThat(result.size(), equalTo(5));
+    }
+
+    @Test
+    public void verificarOracionConCincoPalabrasYEspacioFinal() {
+        String sentence = "Hola mi nombre es robot ";
+
+        List result = ejercicio3.splitSentence(sentence);
+        assertThat(result.size(), equalTo(5));
+    }
+
+    @Test
+    public void verificarOracionConUnaPalabras() {
+        String sentence = "robot";
+
+        List result = ejercicio3.splitSentence(sentence);
+        assertThat(result.size(), equalTo(1));
+    }
+
+    @Test
+    public void verificarOracionConUnEspacio() {
+        String sentence = " ";
+
+        List result = ejercicio3.splitSentence(sentence);
+        assertThat(result.size(), equalTo(0));
+    }
+
+    @Test
+    public void verificarOracionVacia() {
+        String sentence = "";
+
+        List result = ejercicio3.splitSentence(sentence);
+        assertThat(result.size(), equalTo(1));
+    }
+
+}

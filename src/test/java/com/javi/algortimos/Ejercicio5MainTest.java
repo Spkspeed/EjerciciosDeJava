7743880b337ccf890b0c41e182cefceefc541c06
recruz79 <raul.cruz79@gmail.com>
2020-06-13 21:37:38 -0300

    test

---
+++ src/test/java/com/javi/algortimos/Ejercicio5MainTest.java
@@ -0,0 +1,33 @@
+package com.javi.algortimos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+@RunWith(SpringRunner.class)
+
+public class Ejercicio5MainTest {
+
+    Ejercicio5 mensajero = new Ejercicio5();
+
+    @Test
+    public void verificarPalabraEsPalindromo() {
+        //puedes enviar valores al llamar a un metodo y el puede recibirlos con tales valores esperandolo.
+        boolean resultado = mensajero.esPalindromo("anana");
+        assertThat(resultado, equalTo(true));
+
+    }
+
+    @Test
+    public void verificarPalabraNoEsPalindromo() {
+        boolean resultado = mensajero.esPalindromo("pollo");
+        assertThat(resultado , equalTo(false));
+
+    }
+
+}

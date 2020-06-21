f17424bff6d7d4d3ba7bc0895d4eb7a034766959
recruz79 <raul.cruz79@gmail.com>
2020-06-21 15:33:29 -0300

    registroAsistencias mejorado

--- src/test/java/com/javi/poo/registroAsistencias/MainAsistenciasTest.java
+++ src/test/java/com/javi/poo/registroAsistencias/MainAsistenciasTest.java
@@ -18,40 +18,53 @@ public class MainAsistenciasTest{
 
     @Test
     public void testAsistencias() {
+
         Alumno alumnoEma = new Alumno("Emanuel", "Soliz");
         Alumno alumnoJavi = new Alumno("Javier", "Soliz");
         Alumno alumnoAriel = new Alumno("Ariel", "Jarpa Soliz");
         Alumno alumnoAlfredo = new Alumno("Alfredo", "Villegas");
+        Docente docenteRaul = new Docente("Raul", "Soliz");
 
         Aula aulaPrimeroA = new Aula();
-
+        aulaPrimeroA.getListaDocente().add(docenteRaul);
         aulaPrimeroA.getListaAlumnos().add(alumnoEma);
         aulaPrimeroA.getListaAlumnos().add(alumnoJavi);
         aulaPrimeroA.getListaAlumnos().add(alumnoAriel);
 
+
         Asistencia asistencia = new Asistencia(); //forma de llamar al map.
 
+        asistencia.getAsistenciasDocente().put(docenteRaul, true);
         asistencia.getAsistencias().put(alumnoEma, true);
         asistencia.getAsistencias().put(alumnoJavi, true);
         asistencia.getAsistencias().put(alumnoAriel, true);
         asistencia.getAsistencias().put(alumnoAlfredo, true);
 
 
+
         // Una lista puede ser iterada sencillamente con un for
         for(Alumno alumno : aulaPrimeroA.getListaAlumnos()) {
-            System.out.println("Un alumno de primero A es: " + alumno.getNacionalidad() + " " + alumno.getApellido());
+            System.out.println("El docente de primero A es: " + docenteRaul.getNombre() + " " + alumno.getApellido());
+            System.out.println("Un alumno de primero A es: " + alumno.getNombre() + " " + alumno.getApellido());
         }
 
         // Un mapa primero debe obtenerse un iterador y luego se lo podra leer
         for(Map.Entry<Alumno, Boolean> entry : asistencia.getAsistencias().entrySet()) {
-            System.out.println("El alumno de primero A es: " + entry.getKey().getNacionalidad() + " " + entry.getKey().getApellido() + " Falto?? " + entry.getValue());
+            System.out.println("El alumno de primero A es: " + entry.getKey().getNombre() + " " + entry.getKey().getApellido() + " Falto?? " + entry.getValue());
             // Assert que verifica que todos los alumnos hallan estado presentes o sea TRUE
             assertThat(entry.getValue().booleanValue(), equalTo(true));
         }
 
         // Assert que verifica que Aula tenga cuatro alumnos
         assertThat(aulaPrimeroA.getListaAlumnos().size(), equalTo(3));
+        assertThat(aulaPrimeroA.getListaDocente().size(), equalTo(1));
+        Docente docenteResult = aulaPrimeroA.getListaDocente().get(0);
 
+        if(docenteRaul.equals(docenteResult)) {
+            System.out.println("Docente raul y docente result son iguales!");
+        }
+
+        assertThat(docenteResult, equalTo(docenteRaul));
     }
 
 

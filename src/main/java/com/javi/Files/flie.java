c3d9a459a2a9f46d107f489ea06501ed391beae5
Spkspeed <spkspeed@gmail.com>
2020-07-19 12:47:30 -0300

    file

--- src/main/java/com/javi/Files/flie.java
+++ src/main/java/com/javi/Files/flie.java
@@ -19,6 +19,9 @@ class leer_Fichero {
         FileReader entrada = null;
         int A1 = 0, A2 = 0, A3, A4, B1, B2, B3, B4;
         int num_caracteres = 0;
+        int aumentar = 0;
+        String confirmacion = "";
+        
         try {
             entrada = new FileReader("C:\\Users\\recruz\\IdeaProjects\\EjerciciosDeJava\\src\\test\\resources\\ClasesModel.data");
             int c = 0;
@@ -29,12 +32,20 @@ class leer_Fichero {
                     c = entrada.read();
                     char variable = (char) c;
                     acumulacion += variable;
-                    //falta mas programacion
+                    
                     if (variable == '.') {
                         A2 = 1;
                     }
                 }
-                System.out.println(acumulacion);
+                char palabra[] = new char[acumulacion.length()];
+                for(int i = 0; i <= (acumulacion.length() - 1); i++){
+                    palabra [aumentar] = acumulacion.charAt(i);
+                    if(acumulacion.charAt(i) == ','){
+                        confirmacion = acumulacion.substring((i - aumentar), i);
+                        System.out.println(confirmacion);
+                    }
+                    aumentar++;
+                }
                 A2 = 0;
             }
             entrada.close();

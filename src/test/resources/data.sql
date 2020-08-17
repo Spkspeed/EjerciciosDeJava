c7582b562885a1899cf0fe7b7f0a51060ef3aecb
recruz79 <raul.cruz79@gmail.com>
2020-08-17 14:58:08 -0300

    lunes TAREA

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,13 +1,19 @@
-INSERT INTO Alumno (id, nombre, apellido, nacionalidad, edad) VALUES
-  (1, 'Roberto', 'Luz', 'Argentino', 19),
-  (2, 'Carlos', 'Fuentes', 'Argentino', 12),
-  (3, 'Julio', 'Cruz', 'Argentino', 22),
-  (4, 'Robin', 'Soliz', 'Argentino', 19);
-INSERT INTO Preceptor (id, nombre, apellido, nacionalidad) VALUES
-  (5, 'Carto', 'Luz', 'Argentino'),
-  (6, 'Robin', 'Hood', 'Argentino'),
-  (7, 'Carlos', 'Cruz', 'Argentino');
+/*
+ 1.- Cargar todos los alumnos de BaseTest 20 File 20 y SQL 20
+ 2.- Borrar ListaAlumnosExcelentes y mover el mapeo a ListaAlumnos
+ 3.- Mapear Docente ManyToOne a Clase
+ 4.- Mapear Preceptor en Clase averiguar como hacerlo
+ */
 INSERT INTO Clase (id, nombre_clase) VALUES
   (8, 'ClaseA'),
   (9, 'ClaseB'),
   (10, 'ClaseC');
+INSERT INTO Alumno (id, nombre, apellido, nacionalidad, edad, clase_id) VALUES
+  (1, 'Roberto', 'Luz', 'Argentino', 19, 8),
+  (2, 'Carlos', 'Fuentes', 'Argentino', 12, 8),
+  (3, 'Julio', 'Cruz', 'Argentino', 22, 8),
+  (4, 'Robin', 'Soliz', 'Argentino', 19, 8);
+INSERT INTO Preceptor (id, nombre, apellido, nacionalidad) VALUES
+  (5, 'Carto', 'Luz', 'Argentino'),
+  (6, 'Robin', 'Hood', 'Argentino'),
+  (7, 'Carlos', 'Cruz', 'Argentino');

651bf7b75e119784454e73b8708480edbd0a0b85
Javir Soliz <spkspeed@gmail.com>
2021-11-04 22:15:28 -0300

    comentado

--- src/main/java/com/javi/contador/Contador.java
+++ src/main/java/com/javi/contador/Contador.java
@@ -8,28 +8,35 @@ public class Contador {
 
     public void contadorCaracteres (String palabra) {
         for (int i = 0; i < palabra.length(); i++) {
-            for (int y = 0; y < abecedario.length(); y++){
 
+            for (int y = 0; y < abecedario.length(); y++){
                 if (palabra.charAt(i) == abecedario.charAt(y)){
                     System.out.println("-------la posicion anterior es: " + posicionAnterior);
                     System.out.println("-------la posicion actual es: " + posicionActual);
 
-                    int distanciaEntreVariables = posicionActual - posicionAnterior;
+                    //Obtiene la distancia que existe entre la letra actual y la anterior
+                    int distanciaEntreVariables = posicionActual - posicionAnterior; //si no se ejecutan alguno de los siguientes "if", este sera el resultado final que se muestre como distancia.
 
-                    //si el resultado es negativo lo transformo a positivo
+                    //if para cambiar el resultado negativo a positivo porque el valor de la posicion anterior puede ser aveces mayor que el valor del actual
                     if (distanciaEntreVariables < 0){
                         distanciaEntreVariables = -distanciaEntreVariables;
                     }
+                    //para conocer el camino mas corto que se debe tomar para llegar de una letra a otra utilizamos este metodo
                     if (distanciaEntreVariables > 12){
                         System.out.println("la distancia entre variables es mayor a 12, se debe tomar el otro camino.");
+
+                        //formula utilizada para obtener la cantidad de pasos necesarios para llegar a la letra actual.
                         distanciaEntreVariables = 25 - distanciaEntreVariables;
                         System.out.println("distancia corregida: " + distanciaEntreVariables);
-
                     } else {
                         System.out.println("distancia: " + distanciaEntreVariables);
                     }
+
                     posicionAnterior = posicionActual;
-                    posicionActual = 0;
+
+                    //para volverla a colocar en la posicion inicial
+                    posicionActual = 0; //tal vez sea innecesaria y solo tengo que utilizar la variable "y"
+                    //break para evitar que continue analizando las letras restantes del abcdario
                     break;
                 } else{
                     posicionActual++;
